9618fe19015d75c8dddf33fe4563c0889cc97034
Ashish kumarsn <kaumatchobey@gmail.com>
2025-08-11 18:09:44 +0530

    Update changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ import EditCourse from './pages/admin/course/EditCourse';
 import CreateLecture from './pages/admin/lecture/CreateLecture';
 import EditLecture from './pages/admin/lecture/EditLecture';
 import CourseDetail from './pages/student/CourseDetail';
-import CourseProgress from './pages/student/courseProgress';
+import CourseProgress from './pages/student/CourseProgress';
 import SearchPage from './pages/student/SearchPage';
 
 import CourseCategories from './components/CourseCategories';
